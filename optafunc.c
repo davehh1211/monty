d7b305774e637be35c2a2325273b01de9bd69ba8
davehh1211 <davehh1211@gmail.com>
2020-05-12 23:39:45 +0000

    struct of functions

---
+++ optafunc.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+/*
+*
+*
+*/
+void optacodevalid(char *token, stack_t **stack, unsigned int line_number )
+{
+instruction_t optaexist[] = {
+		{"push", o_push},
+		{"pall", o_pall},
+		{"pint", o_pint},
+		{"pop", o_pop},
+		{"swap", o_swap},
+		{"add", o_add},
+		{"nop", o_nop},
+		{"sub", o_sub},
+		{"div", o_div},
+		{"mul", o_mul},
+		{"mod", o_mod},
+		{"pchar", o_pchar},
+		{NULL, NULL}
+    };
+
+for (i = 0; optaexist[i].opcode != NULL; i++)
+{
+	if (token == optaexist[i].opcode)
+	{
+		return (optaexit[i].(*f)( **stack, line_number);)
+	}
+}
+if (! optaexist[i].opcode)
+{
+	dprintf(STDOUT_FILENO, "L%d: unknown instruction %s", line_number, token);
+	exit(EXIT_FAILURE);
+}
+
+}
