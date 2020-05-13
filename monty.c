959d499390545769a13e43cfb5efaed031689265
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-13 02:18:53 +0000

    Main file V1

--- monty.c
+++ monty.c
@@ -1,4 +1,4 @@
-#include "montyheader.h"
+#include "monty.h"
 /**
  * 
  * 
@@ -6,31 +6,54 @@
 int main(int argc, char *argv[])
 {
 	FILE *filename = NULL;
-	int i;
-	char *buffer = NULL, *token = NULL;
+	unsigned int line_number = 1, i = 0;
+	char *buffer = NULL, *token = NULL, **arguments = NULL;
 	size_t n = 0;
-	stack_t **stack = NULL;
-
+	/*stack_t **stack = NULL;*/
 
 	if (argc != 2)
 	{
-		dprintf(STOUT_FILENO, "USAGE: monty file");
+		dprintf(STDOUT_FILENO, "USAGE: monty file");
 		exit(EXIT_FAILURE);
 	}
 	filename = fopen(argv[1], "r");
 	if (filename == NULL)
 	{
-		dprintf(STOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	i = getline(&buffer, &n, filename);
-	if (i < 0)
-		free(buffer);
-	token = parse_buffer(buffer) /*COMO PARSEAMOS*/
-	/*strtok(buffer, "\n\t\r ");*/
-	if (token != NULL)
+	while (getline(&buffer, &n, filename) != -1)
 	{
-		optacodevalid(token, &stack, line_number ));
+		/* check if it need a buffer duplicated */
+		arguments = malloc(3 * sizeof(char *));
+		if (arguments == NULL)
+				exit(EXIT_FAILURE);
+		token = strtok(buffer, "\n\t ");
+		while (token != NULL)
+		{
+			arguments[i] = malloc(sizeof(char));
+			if (arguments[i] == NULL)
+			{
+				free(arguments);
+				exit(EXIT_FAILURE);
+			}
+			strncpy(arguments[i], token, strlen(token) + 1);
+			token = strtok(NULL, "\n\t ");
+			i++;
+		}
+		arguments[i] = '\0';
+		for (i = 0; arguments; i++)
+			printf("%s\n", arguments[i]);
+		for (i = 0; arguments; i++)
+			free(arguments[i]);
+		free(arguments);
+		/*if (token != NULL)
+		{
+			optacodevalid(token, &stack, line_number);
+		}*/
+		line_number++;
 	}
+	free(buffer);
 	exit(EXIT_SUCCESS);
 }
+
