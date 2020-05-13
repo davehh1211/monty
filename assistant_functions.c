317d8df40e1d825ae75e95bcafb3092dac992861
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-13 15:46:43 +0000

    Monty interpreter update - parse line and send to opfuncions

---
+++ assistant_functions.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * _calloc - Allocates memory for an array of nmemb elements of size bytes.
+ * @nmemb: Number of elements of the array.
+ * @size: size bytes of the element type.
+ * Return: Pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+        char *s;
+        unsigned int i;
+
+        if (nmemb == 0 || size == 0)
+                return (NULL);
+
+        s = malloc(nmemb * size);
+        if (s == NULL)
+                return (NULL);
+
+        for (i = 0; i < (size * nmemb); i++)
+                s[i] = 0;
+        return (s);
+}
--- monty.c
+++ monty.c
@@ -1,14 +1,16 @@
 #include "monty.h"
 /**
- * 
- * 
+ * main - 
+ * @argc:
+ * @argv:
+ * Return: 
  */
 int main(int argc, char *argv[])
 {
 	FILE *filename = NULL;
-	unsigned int line_number = 1, i = 0;
-	char *buffer = NULL, *token = NULL, **arguments = NULL;
-	size_t n = 0;
+	unsigned int line_number = 1;
+	char *buffer = NULL, *opcode = NULL, *num;
+	size_t len = 0;
 	/*stack_t **stack = NULL;*/
 
 	if (argc != 2)
@@ -22,38 +24,16 @@ int main(int argc, char *argv[])
 		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (getline(&buffer, &n, filename) != -1)
+	while (getline(&buffer, &len, filename) != -1)
 	{
-		/* check if it need a buffer duplicated */
-		arguments = malloc(3 * sizeof(char *));
-		if (arguments == NULL)
-				exit(EXIT_FAILURE);
-		token = strtok(buffer, "\n\t ");
-		while (token != NULL)
-		{
-			arguments[i] = malloc(sizeof(char));
-			if (arguments[i] == NULL)
-			{
-				free(arguments);
-				exit(EXIT_FAILURE);
-			}
-			strncpy(arguments[i], token, strlen(token) + 1);
-			token = strtok(NULL, "\n\t ");
-			i++;
-		}
-		arguments[i] = '\0';
-		for (i = 0; arguments; i++)
-			printf("%s\n", arguments[i]);
-		for (i = 0; arguments; i++)
-			free(arguments[i]);
-		free(arguments);
-		/*if (token != NULL)
-		{
-			optacodevalid(token, &stack, line_number);
-		}*/
+		opcode = strtok(buffer, "\n\t ");
+		num = strtok(NULL, "\n\t ");
 		line_number++;
+		/*printf("%s - %s\n", opcode, num);*/
+		if (opcode != NULL)
+			optacodevalid(opcode, num, line_number);
 	}
+	/*free(filename); This free is neccessary but it send us some errors*/
 	free(buffer);
 	exit(EXIT_SUCCESS);
 }
-
