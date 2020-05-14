1c16bff4d9b6216a8a3f858c1a8e09a9c078c065
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-14 16:41:33 +0000

    function nop file

--- o_nop.c
+++ o_nop.c
@@ -2,7 +2,7 @@
 /**
  * o_nop - doesn't do anything (linked list)
  * @stack: the stack to do void
- * @line_number: the  line of the file read
+ * @line_number: the line of the file read
  * Return: nothing
  */
 void o_nop(stack_t **stack, unsigned int line_number)
